5ccaf9bd80a8627bc14fedd22d20ac5a49d4d40d
fpabl0 <f.pablo1@hotmail.com>
2025-02-26 00:56:43 -0500

    postly app now points to kong api gateway

--- postly_app/lib/notifiers/_consts.dart
+++ postly_app/lib/notifiers/_consts.dart
@@ -1,4 +1,4 @@
-const graphApiUrl = "http://10.0.2.2:8080/query";
+const graphApiUrl = "http://10.0.2.2:8000/query";
 const auth0Domain = "dev-zwb0m3gcvya7dcz5.us.auth0.com";
 const auth0ClientId = "Yi1rAUmG7Cww9T4g7lPinyMu17mRI6dU";
 const auth0Scheme = "msoft.g1.postlyapp";
